971342c94b3f2bc5fec6a0599f5cdfc4f3b0be49
IsaacLeen lee <Isaacharrisonlee@gmail.com>
2024-09-28 07:18:31 +0000

    done

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 """
-Runs the main game loop for the game engine.
+Runs the main game loop for the game engine. 
 
 This script is the entry point for the game application. It creates a new instance of the `GameEngine` class and calls its `play()` method to start the game loop.
 """
+#stussy
 # main.py
 from game_engine import GameEngine
 
